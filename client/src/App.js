d73d00062b4032838006ae6ccf298a9ad4a3ff43
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-12-07 22:59:00 +0400

    Setup basic Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
 
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
-import './App.css';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
