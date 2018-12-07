9cb099d0742427fe016eb9873c538b879513476c
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-12-07 16:28:02 +0400

    Setup basic react

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on ${port}!`);
 });
